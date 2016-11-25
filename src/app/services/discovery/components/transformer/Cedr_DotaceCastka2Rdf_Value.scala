068d3a628cc7f6d8d7f766e04132c365ff7c4b60
Jiří Helmich <jiri@helmich.cz>
2016-11-25 09:52:24 +0100

    Update Cedr_DotaceCastka2Rdf_Value.scala

--- src/app/services/discovery/components/transformer/Cedr_DotaceCastka2Rdf_Value.scala
+++ src/app/services/discovery/components/transformer/Cedr_DotaceCastka2Rdf_Value.scala
@@ -5,7 +5,10 @@ class Cedr_DotaceCastka2Rdf_Value extends SparqlUpdateTransformer {
     protected override val prefixes =
         """
           | PREFIX cedr: <http://cedropendata.mfcr.cz/c3lod/cedr/vocabCEDR#>
-          | PREFIX dct: <http://purl.org/dc/terms/>""".stripMargin
+          | PREFIX dct: <http://purl.org/dc/terms/>
+          | PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+          | PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+          | PREFIX lpviz: <http://visualization.linkedpipes.com/ontology/>""".stripMargin
     
     protected override val deleteClause = """?dotace cedr:castkaRozhodnuta ?castka ."""
     
