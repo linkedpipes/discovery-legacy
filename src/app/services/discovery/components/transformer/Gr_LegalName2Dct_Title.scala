d634de5f4df41c77ef1e5135c7585a2c6cface93
Jiří Helmich <jiri@helmich.cz>
2016-12-12 09:53:23 +0100

    transformer updates

--- src/app/services/discovery/components/transformer/Gr_LegalName2Dct_Title.scala
+++ src/app/services/discovery/components/transformer/Gr_LegalName2Dct_Title.scala
@@ -2,17 +2,24 @@ package services.discovery.components.transformer
 
 class Gr_LegalName2Dct_Title extends SparqlUpdateTransformer {
 
-    protected override val deleteClause = "?be gr:legalName ?name ."
-    protected override val whereClause =
-        """
-          |?be gr:legalName ?name .""".stripMargin
-    protected override val insertClause =
-        """
-          |?be dct:title ?name .
-        """.stripMargin
     protected override val prefixes =
         """
           |PREFIX gr: <http://purl.org/goodrelations/v1#>
           |PREFIX dct: <http://purl.org/dc/terms/>
+        """.stripMargin
+
+    protected override val deleteClause = "?be gr:legalName ?name ."
+
+    protected override val insertClause =
+        """
+          |?be dct:title ?name .
+        """.stripMargin
+
+    protected override val whereClause =
+        """
+          |  ?be gr:legalName ?name .
+          |  FILTER NOT EXISTS {
+          |    ?be dct:title ?name .
+          |  }
         """.stripMargin
 }
--- src/app/services/discovery/components/transformer/Movie_InitialReleaseOf2Time_Instant.scala
+++ src/app/services/discovery/components/transformer/Movie_InitialReleaseOf2Time_Instant.scala
@@ -13,7 +13,8 @@ class Movie_InitialReleaseOf2Time_Instant extends SparqlUpdateTransformer {
 
     protected override val deleteClause =
         """
-          |?m movie:initial_release_date ?dateTime .
+          |?m movie:initial_release_date ?date ;
+          |    dct:date ?date .
         """.stripMargin
 
     protected override val insertClause =
@@ -21,7 +22,7 @@ class Movie_InitialReleaseOf2Time_Instant extends SparqlUpdateTransformer {
           |?m lpviz:hasAbstraction ?abstraction .
           |
           |  ?abstraction a time:Instant ;
-          |    time:inXSDDateTime ?dateTime ;
+          |    time:inXSDDateTime ?date ;
           |	rdfs:label ?abstractionLabel .
         """.stripMargin
 
@@ -30,8 +31,12 @@ class Movie_InitialReleaseOf2Time_Instant extends SparqlUpdateTransformer {
           |?m movie:initial_release_date ?date .
           |
           |  OPTIONAL {
-          |	?s dct:title ?title .
+          |	?m dct:title ?title .
           |	 BIND(CONCAT("Initial release date of ", STR(?title)) AS ?abstractionLabel)
+          |  }
+          |
+          |  OPTIONAL {
+          |    ?m dct:date ?date .
           |  }
           |
           |  BIND(IRI(CONCAT(STR(?m), "/abstraction/linkedmdb-initial-release-of2time-Instant")) AS ?abstraction)
