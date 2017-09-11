9ce6f912789dfa386dfd1e4be40f8c4c8d83068b
Jiří Helmich <jiri@helmich.cz>
2017-09-11 21:55:37 +0200

    DCTerms app

--- scripts/run-appgen.sh
+++ scripts/run-appgen.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/matrix.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/conceptscheme.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/geo-geocoordinates.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/geo-place.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/geo-quantified-place.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/geo-quantified-thing.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/matrix.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/time-instant.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/time-interval.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/time-thing-with-instant.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/time-thing-with-interval.sparql"
-scala appgen.scala "app-uri-id" "App Title" "App Description" "../data/rdf/applications/v02/time-versioned-thing.sparql"
+scala appgen.scala "dcterms" "DCTerms Application" "App that displays dcterms instances" "../data/rdf/applications/v02/dcterms-klima.sparql"
