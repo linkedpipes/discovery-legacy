a016404a667184f5ba1d56a3b641db633da631db
Jirka Helmich <jirka@mewssystems.com>
2018-11-29 12:10:03 +0100

    gather data

--- src/gen/gather-stats.scala
+++ src/gen/gather-stats.scala
@@ -2,19 +2,23 @@ import better.files._
 
 import scala.io.Source
 
-val baseFolder = "D:\\mff-experiments"
-val experiments = baseFolder.toFile.list(f => f.isDirectory)
-val header1csv = "\"Discovery ID\",\"Discovery URI\",\"Application group count\",\"Datasource group count\",\"Extractor group count\",\"Data sample group count\",\"Pipeline count\",\"Discovery duration\",\"Application count\",\"Data source count\",\"Transformer count\""
+val baseFolder = "D:\\mff-experiments\\"
+val experiments = baseFolder.toFile.children.toList
+val header1csv = "\"Experiment\",\"Discovery ID\",\"Discovery URI\",\"Application group count\",\"Datasource group count\",\"Extractor group count\",\"Data sample group count\",\"Pipeline count\",\"Discovery duration\",\"Application count\",\"Data source count\",\"Transformer count\""
 
 
 val allLines = for {
     i <- 0 to 47
 } yield experiments.map { e =>
-    val discoveryPath = s"{$baseFolder}\\dis-${"%03d".format(i)}\\1.csv"
-    val lines = Source.fromFile(discoveryPath).getLines().toSeq
-    lines.last
+    val discoveryPath = s"$baseFolder\\${e.name}\\dis-${"%03d".format(i)}\\1.csv"
+    if (discoveryPath.toFile.exists){
+        val lines = Source.fromFile(discoveryPath).getLines().toSeq
+        s"${e.name},${lines.last}"
+    } else {
+        ""
+    }
 }
 
-val content = Seq(header1csv) ++ allLines.flatten
+val content = Seq(header1csv) ++ allLines.flatten.filterNot(l => l.isEmpty)
 
 s"$baseFolder\\1.csv".toFile.createFileIfNotExists().writeText(content.mkString("\n"))
